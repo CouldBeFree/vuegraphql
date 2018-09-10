bf90b935758baec03a4bed9fd67ad075db59bbc8
Explorer <propokerstar90@gmail.com>
2018-09-10 22:46:27 +0300

    Enabled collecting data from form

--- client/src/components/AddBook.vue
+++ client/src/components/AddBook.vue
@@ -1,26 +1,33 @@
 <template>
-    <div>
-      <form id="add-book">
-        <div class="field">
-          <label>Book name:</label>
-          <input type="text">
-        </div>
-        <div class="field">
-          <label>Genre:</label>
-          <input type="text">
-        </div>
-        <div class="field">
-          <label>Author:</label>
-          <select v-if="$apolloData.queries.authors.loading">
-            <option disabled>Authors loading</option>
-          </select>
-          <select v-else>
-            <option v-for="author in authors" :key="author.id" :value="author.id">{{author.name}}</option>
-          </select>
-        </div>
-        <button>+</button>
-      </form>
-    </div>
+  <div>
+    <form id="add-book">
+      <div class="field">
+        <label>Book name:</label>
+        <input type="text" v-model="name">
+      </div>
+      <div class="field">
+        <label>Genre:</label>
+        <input type="text" v-model="genre">
+      </div>
+      <div class="field">
+        <label>Author:</label>
+        <select v-if="$apolloData.queries.authors.loading">
+          <option disabled>Authors loading</option>
+        </select>
+        <select v-else v-model="selectedAuthor">
+          <option disabled value="">Please select author</option>
+          <option
+            v-for="author in authors"
+            :key="author.id"
+            :value="author.id"
+          >
+            {{author.name}}
+          </option>
+        </select>
+      </div>
+      <button @click.prevent="getData">+</button>
+    </form>
+  </div>
 </template>
 
 <script>
@@ -35,19 +42,33 @@
     }
   `;
 
-    export default {
-        name: "AddBook",
-      data(){
-        return{
-          authors:[]
-        }
-      },
-      apollo: {
-        authors:{
-          query: getAuthorQuery
-        }
+  export default {
+    name: "AddBook",
+    data(){
+      return{
+        authors:[],
+        name:'',
+        genre:'',
+        selectedAuthor:'',
+        authorId:''
+      }
+    },
+    apollo: {
+      authors:{
+        query: getAuthorQuery
+      }
+    },
+    methods:{
+      getData(){
+        let data = {
+          name: this.name,
+          genre: this.genre,
+          id: this.selectedAuthor
+        };
+        console.log(data);
       }
     }
+  }
 </script>
 
 <style scoped>
